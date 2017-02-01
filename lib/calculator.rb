aafae1a03808377f5cef9b4983af6a398e0df87f
Sharan Kaur <sharanvkaur@gmail.com>
2017-02-01 15:37:28 +0800

    :tada:

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,70 @@
 class Calculator
-  # your class goes here
+  attr_reader :result
+  attr_accessor :history
+  attr_accessor :undonehistory
+
+  def initialize(result)
+    @result = result
+    @history
+    @undonehistory
+  end
+
+  def result
+    @result
+  end
+
+  def reset(x)
+    @result = x
+  end
+
+  def add(x)
+    @history = @result
+    @result += x
+    self
+  end
+
+  def subtract(x)
+    @history = @result
+    @result -= x
+    self
+  end
+
+  def multiply(x)
+    @history = @result
+    @result *= x
+    self
+  end
+  def divide(x)
+    @history = @result
+    @result /= x
+    self
+  end
+
+  def operation(op, x)
+    @history = @result
+    case
+      when op == "add"
+        @result += x
+      when op == "subtract"
+        @result -= x
+      when op == "multiply"
+        @result *= x
+      when op == "divide"
+        @result /= x
+    end
+  end
+
+  def undo
+    @undonehistory = @result
+    @result = @history
+  end
+
+  def redo
+    if (@undonehistory == nil)
+      @result = @result
+    else
+      @result = @undonehistory
+      @undonehistory = nil
+    end
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,10 +1,76 @@
 require_relative 'spec_helper'
-require_relative '../lib/calculator'
+require_relative '../lib/calculator.rb'
 
 describe Calculator do
-  before(:context) do
-    # initialize calculator
+
+  before(:example) do
+    @calculator = Calculator.new(10)
   end
 
-  # Tests go here
+  describe 'Initialization' do
+    it 'is an instance of the Calculator class' do
+      expect(@calculator).to be_instance_of(Calculator)
+    end
+
+    it 'should get current result' do
+      expect(@calculator.result).to eq(10)
+    end
+
+    it "should be read-only" do
+      expect{@calculator.result = 4}.to raise_error(NoMethodError)
+    end
+  end
+
+  describe 'Methods' do
+    it 'should reset calculator' do
+      @calculator.reset(10)
+      expect(@calculator.result).to eq(10)
+    end
+
+    it "should add" do
+      @calculator.add(5)
+      expect(@calculator.result).to eq(15)
+    end
+
+    it "should subtract" do
+      @calculator.subtract(5)
+      expect(@calculator.result).to eq(5)
+    end
+
+    it "should multiply" do
+      @calculator.multiply(10)
+      expect(@calculator.result).to eq(100)
+    end
+
+    it "should divide" do
+      @calculator.divide(2)
+      expect(@calculator.result).to eq(5)
+    end
+
+    it "ensure all methods are chainable" do
+      @calculator.multiply(2).add(5)
+      expect(@calculator.result).to eq(25)
+    end
+
+    it "should apply operation with the value of x" do
+      @calculator.operation('add', 1000)
+      expect(@calculator.result).to eq(1010)
+    end
+
+    it "cannot redo because no undo" do
+      @calculator.redo()
+      expect(@calculator.result).to eq(10)
+    end
+
+     it "should undo one operation restoring the result of the previous operation" do
+       @calculator.add(10).undo()
+       expect(@calculator.result).to eq(10)
+     end
+
+     it "should redo previously undone operation" do
+       @calculator.add(10).undo()
+       @calculator.redo()
+       expect(@calculator.result).to eq(20)
+     end
+  end
 end
